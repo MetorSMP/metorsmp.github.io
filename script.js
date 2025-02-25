ddf7cba826db3733a092524abd788b2a43b21bca
MetorSMP <contact.metorsmp@gmail.com>
2025-02-25 22:11:22 +0100

    main commit
    This commit add the main website files to repository. (Actually this is the first version of website).
    [#######################################################################]
    
    Include next files:
    
    = index.html : main website html file
    [///////////////////////////////////////////]
    = style.css : main stylesheet file, that contains WHOLE WEBSITE STYLE DATA
    [///////////////////////////////////////////]
    = script.js : actualy not in use rn. Yes, its connected to index.html and have some data, but website didnt use this data at the moment of creation.
    [///////////////////////////////////////////]

---
+++ script.js
@@ -0,0 +1,37 @@
+new FinisherHeader({
+    "count": 12,
+    "size": {
+      "min": 1300,
+      "max": 1500,
+      "pulse": 0
+    },
+    "speed": {
+      "x": {
+        "min": 0.6,
+        "max": 3.5
+      },
+      "y": {
+        "min": 0.6,
+        "max": 3.5
+      }
+    },
+    "colors": {
+      "background": "#582a81",
+      "particles": [
+        "#511cff",
+        "#ca87fe",
+        "#a31efe",
+        "#fd0aff"
+      ]
+    },
+    "blending": "lighten",
+    "opacity": {
+      "center": 0.6,
+      "edge": 0
+    },
+    "skew": -2,
+    "shapes": [
+      "c"
+    ]
+  });
+  
